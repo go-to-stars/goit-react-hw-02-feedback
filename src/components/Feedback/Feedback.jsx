22646e3d01539eef73ec2070e5f250909e388f50
go-to-stars <ivanstep@ukr.net>
2023-07-04 18:10:07 +0300

    removed the transit component

--- src/components/Feedback/Feedback.jsx
+++
@@ -1,7 +0,0 @@
-// import { FeedbackOptions } from "../FeedbackOptions/FeedbackOptions"; // імпорт компоненту FeedbackOptions
-
-// export const Feedback = ({ options, onLeaveFeedback }) => {
-//   return (
-//     <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
-//   );
-// };
